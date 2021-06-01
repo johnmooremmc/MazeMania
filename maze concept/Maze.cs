96c892d8d1d44c9700f73d044d48d847adda2a8a
John Moore <johnmoore@mmc.school.nz>
2021-06-02 10:59:02 +1200

    Add project files.

---
+++ maze concept/Maze.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maze_concept
+{
+    class Maze
+    {
+
+        public int Rows;
+        public int Columns;
+
+        public Maze(int rows, int columns)
+        {
+            int x;
+            int y;
+
+
+            Rows = rows;
+            Columns = columns;
+
+
+
+        }
+    }
+}
