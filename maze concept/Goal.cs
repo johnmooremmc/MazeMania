7a62123c7896fbfd521b7cfe3a711a68a146ad3e
John Moore <johnmoore@mmc.school.nz>
2021-07-20 16:10:49 +1200

    goal.cs commented

--- maze concept/Goal.cs
+++ maze concept/Goal.cs
@@ -11,18 +11,20 @@ namespace maze_concept
     {
         public int width, height;
         public int x, y;
-
+        // defines public integer values for later use
         
 
+        // below is a method which can be called on from form1.cs  which will be required to send defined graphics and the image source
         public void DrawGoal(Graphics g, Image goalSkin)
         {
 
             width = 45;
             height = 45;
+            // integer valules for width and height are defined
             
 
             g.DrawImage(goalSkin, x + 2, y + 2, width, height);
-            
+            // values used to draw a rectangle with an image picture
 
         }
 
